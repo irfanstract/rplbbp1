67bdbc194c8a19555bdf90790a167118b0f2f7e4
meh <meh@meh>
2023-03-15 22:30:50 +0700

    `src\_x\bbpy\util\debugging.ts` - add re-exports from `node:assert`

--- src/_x/bbpy/util/debugging.ts
+++ src/_x/bbpy/util/debugging.ts
@@ -8,6 +8,26 @@
 
 
 
+/* module uses `export = ...` ; can't simply use `export * from it` */
+export {
+  AssertPredicate ,
+  AssertionError ,
+  CallTracker ,
+  CallTrackerReportInformation ,
+  deepStrictEqual ,
+  doesNotReject ,
+  doesNotThrow ,
+  fail ,
+  ifError ,
+  notDeepStrictEqual ,
+  notStrictEqual ,
+  ok ,
+  rejects ,
+  strict, 
+  strictEqual ,
+  throws ,
+} from "assert" ;
+
 
 
 
